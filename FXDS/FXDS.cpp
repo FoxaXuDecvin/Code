df84c2e64c3a73eb98eed083fd7901bf0ab50a0c
FoxaXuDecvin <114748519+FoxaXuDecvin@users.noreply.github.com>
2023-01-06 19:33:58 +0800

    Add files via upload

--- FXDS/FXDS.cpp
+++ FXDS/FXDS.cpp
@@ -28,13 +28,68 @@ bool isFileExists_ifstream(string& name) {
 
 int main()
 {
-	string durl = getenv("setURL");
-	string dsp = getenv("setSP");
+	system("echo=%addcode% >>TestDialog.data");
+	string AddCode = "null";
+	ifstream OpenReadAddCode;
+	OpenReadAddCode.open("TestDialog.data");
+	OpenReadAddCode >> AddCode;
+	OpenReadAddCode.close();
+	system("del TestDialog.data");
 
+	string dsp = "nul";
+	string durl = "nul";
+
+	if (AddCode == "%addcode%") {
+		//兼容模式
+		system("echo=%setURL% >>surl.data");
+		system("echo=%setSP% >>ssp.data");
+
+		ifstream OSURL;
+		OSURL.open("surl.data");
+		OSURL >> durl;
+		OSURL.close();
+
+		ifstream OSSP;
+		OSSP.open("ssp.data");
+		OSSP >> dsp;
+		OSSP.close();
+
+		system("del surl.data");
+		system("del ssp.data");
+		
+		if (durl == "%setURL%") {
+			goto NullTask;
+		}
+		if (dsp == "%setSP%") {
+			goto NullTask;
+		}
+		goto StartDownload;
+
+	NullTask:
+		MessageBox(0, L"参数无效，请检查 %setURL%与 %serSP% 两个值是否正确", L"Download Task", MB_OK);
+		return 0;
+	}
+	if (AddCode == "longlink") {
+		string CodeMode = "Long Link";
+		ifstream OpenFileDURL;
+		OpenFileDURL.open("durl.data");
+		OpenFileDURL >> durl;
+		OpenFileDURL.close();
+
+		ifstream OpenFileDSP;
+		OpenFileDSP.open("dsp.data");
+		OpenFileDSP >> dsp;
+		OpenFileDSP.close();
+		system("del durl.data");
+		system("del dsp.data");
+		goto StartDownload;
+	}
+
+	StartDownload:
 	LPCWSTR LcDsp = stringToLPCWSTR(dsp);
 	LPCWSTR LcDURL = stringToLPCWSTR(durl);
 
-	cout << "下载开始 - 下载链接:" << durl << "保存地址: " << dsp << endl;
+	cout << "下载开始  " << AddCode << "  - 下载链接:" << durl << "保存地址 : " << dsp << endl;
 
 	HRESULT applydownload = URLDownloadToFileW(
 		nullptr,
